67ebbdf802b31339c729cc925c646b61a92bbf33
sefinek24 <sefinek5916@gmail.com>
2023-06-11 23:56:01 +0200

    0.2.0

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 require('dotenv').config({ path: './www/config/.env' });
 
 const cluster = require('node:cluster');
-// const totalCPUs = require('node:os').cpus().length;
+const totalCPUs = require('node:os').cpus().length;
 
 if (process.env.NODE_ENV === 'development') {
 	require('./server.js');
 } else if (cluster.isMaster) {
 	console.log(`Primary ${process.pid} is running: http://127.0.0.1:${process.env.PORT}`);
 
-	// for (let i = 0; i < totalCPUs; i++) {
-	// 	cluster.fork();
-	// }
-	//
-	// cluster.on('exit', worker => {
-	// 	console.log(`Worker ${worker.process.pid} died`);
-	// });
+	for (let i = 0; i < totalCPUs; i++) {
+		cluster.fork();
+	}
+
+	cluster.on('exit', worker => {
+		console.log(`Worker ${worker.process.pid} died`);
+	});
 } else {
 	require('./server.js');
 
