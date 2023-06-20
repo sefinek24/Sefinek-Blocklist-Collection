ef029a6a4ebe436a3c224d4e14affeca7bc8526c
sefinek24 <sefinek5916@gmail.com>
2023-06-20 04:53:20 +0200

    Add blocklist.sh

---
+++ cron/blocklist.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+repo_path="/home/ubuntu/node/www/Sefinek-Blocklist-Collection"
+output_file="/home/ubuntu/node/www/Sefinek-Blocklist-Collection/logs/pull_$(date +'%Y-%m-%d').log"
+
+# Change to the repository directory
+cd "$repo_path"
+
+# Write the full date and time to the output file
+echo "\n\n== Date and time: $(date +'%Y-%m-%d %H:%M:%S') ==\n" >> "$output_file"
+
+# Execute the git fetch command and append the output to the file
+git fetch >> "$output_file" 2>&1
+
+# Execute the git pull command and append the output to the file
+git pull >> "$output_file" 2>&1
+
+echo "Output file has been created: $output_file"
