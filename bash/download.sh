71b837cea4150d0b19a9854d979414aa64aba2ac
sefinek24 <sefinek5916@gmail.com>
2023-07-10 05:14:18 +0200

    v0.7.0

--- bash/download.sh
+++ bash/download.sh
@@ -102,14 +102,15 @@ urls=(
 
 
 
+
 for url in "${urls[@]}"; do
   url_parts=(${url//,/ })
   download_url=${url_parts[0]}
   filename=${url_parts[1]}
 
-  wget --progress=bar:force -U "Mozilla/5.0 (compatible; SefinekBlocklistCollection/0.0.0.0; +https://blocklist.sefinek.net)" -P "$output_dir" --no-check-certificate -O "$output_dir/$filename" "$download_url" 2>&1 | \
+  wget --show-progress -U "Mozilla/5.0 (compatible; SefinekBlocklistCollection/0.0.0.0; +https://blocklist.sefinek.net)" -P "$output_dir" --no-check-certificate -O "$output_dir/$filename" "$download_url" 2>&1 | \
   while IFS= read -r line; do
-    if [[ $line == *[%\ ]* ]]; then
+    if [[ $line == *%* ]]; then
       echo -ne "\033[2K\r$line"
     else
       echo "$line"
@@ -130,8 +131,6 @@ for url in "${urls[@]}"; do
     echo "✖ An error occurred during download (status code: $http_status)."
   fi
 
-  echo
-  echo
+  echo ""
+  echo ""
 done
-
-echo "✔ Success! Finished at: $(date +'%Y-%m-%d %H:%M:%S')"
