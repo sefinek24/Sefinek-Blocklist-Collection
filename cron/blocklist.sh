e480d86e818d4bd4566758cb7358b8080b688c20
sefinek24 <sefinek5916@gmail.com>
2023-07-10 03:59:14 +0200

    v0.7.0

--- cron/blocklist.sh
+++ cron/blocklist.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 repo_path="/home/ubuntu/node/www/Sefinek-Blocklist-Collection"
-output_file="$repo_path/www/public/logs/pull_$(date +'%Y-%m-%d').log"
+output_file="/home/ubuntu/node/www/Sefinek-Blocklist-Collection/www/public/logs/pull_$(date +'%Y-%m-%d').log"
 
 # Change to the repository directory
 cd "$repo_path"
 
 # Write the full date and time to the output file
-echo "\n\n== Date and time: $(date +'%Y-%m-%d %H:%M:%S') ==\n" >> "$output_file"
+echo "================================ $(date +'%Y-%m-%d %H:%M:%S') ================================\n" >> "$output_file"
 
-# Execute the git fetch command and append the output to the file
+# GitHub
+git -v >> "$output_file" 2>&1
 git fetch >> "$output_file" 2>&1
-
-# Execute the git pull command and append the output to the file
 git pull >> "$output_file" 2>&1
 
-echo "Output file has been created: $output_file"
+# Final
+echo "\n\n" >> "$output_file" 2>&1
+echo "Done. Output file has been created: $output_file"
