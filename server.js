e4cbb870127070896e8992a78662c068dbc343a9
sefinek24 <sefinek5916@gmail.com>
2023-07-10 06:04:08 +0200

    v0.7.0

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ const app = express();
 app.set('trust proxy', 1);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'www', 'views'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Use
 app.use(cors({ origin: true }));
