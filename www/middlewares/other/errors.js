4e517e136c5c71b898c461f1b5d02f857db77ec2
Sefinek <sefinek5916@gmail.com>
2023-08-11 21:38:28 +0200

    Update

--- www/middlewares/other/errors.js
+++ www/middlewares/other/errors.js
@@ -1,19 +1,19 @@
 const increment = require('../../utils/increment.js');
 
 exports.notFound = async (req, res) => {
-	res.status(404).send('<h1>404 Page was not found 📃</h1><a href="/">Go to Home page...</a>');
+	res.status(404).render('errors/404.ejs');
 
 	await increment.errors(404);
 };
 
 exports.rateLimit = async (req, res) => {
-	res.status(429).send('<h1>429 Too many requests 😑</h1>Please try again later.');
+	res.status(429).render('errors/429.ejs');
 
 	await increment.errors(429);
 };
 
 exports.internalError = async (err, req, res, next) => {
-	res.status(500).send('<h1>500 Server error 😾</h1>');
+	res.status(500).render('errors/500.ejs');
 	await increment.errors(500);
 
 	if (err) console.error(err);
@@ -21,7 +21,7 @@ exports.internalError = async (err, req, res, next) => {
 };
 
 exports.onTimeout = async (req, res) => {
-	res.status(503).send('<h1>503 Woah, timeout ⏰</h1>');
+	res.status(503).render('errors/503.ejs');
 
 	await increment.errors(503);
 };
