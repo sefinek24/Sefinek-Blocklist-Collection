2fdcae54d4e10a3206c7aae3b0e757a7905f7505
Sefinek <sefinek5916@gmail.com>
2023-08-23 13:56:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 require('dotenv').config();
-
 const cluster = require('node:cluster');
-const totalCPUs = require('node:os').cpus().length;
+const os = require('node:os');
 const path = require('node:path');
 const fs = require('fs');
 
-if (!process.env.NODE_ENV || !process.env.DOMAIN || !process.env.PORT) throw new Error('env variables are null or undefined');
-if (!process.env.MONGODB_URL) throw new Error('The MongoDB database URL `MONGODB_URL` was not found in the .env file');
-if (!process.env.SEFINEK_API) throw new Error('Specify the `SEFINEK_API` env variable. For example: https://api.sefinek.net/api/v2');
 
+const totalCPUs = os.cpus().length;
+const logsPath = path.join(__dirname, 'www', 'public', 'logs');
+
+if (!process.env.NODE_ENV || !process.env.DOMAIN || !process.env.PORT) {
+	throw new Error('Environment variables are null or undefined');
+}
+
+if (!process.env.MONGODB_URL) {
+	throw new Error('The MongoDB database URL `MONGODB_URL` was not found in the .env file');
+}
+
+if (!process.env.SEFINEK_API) {
+	throw new Error('Specify the `SEFINEK_API` environment variable, e.g., https://api.sefinek.net/api/v2');
+}
 
 if (process.env.NODE_ENV === 'development') {
+	createDir();
 	require('./www/server.js');
 } else if (cluster.isMaster) {
-	console.log(`Primary ${process.pid} is running: http://127.0.0.1:${process.env.PORT}`);
+	createDir();
+	console.log(`Primary ${process.pid} is running`);
 
 	for (let i = 0; i < totalCPUs; i++) {
 		cluster.fork();
@@ -24,20 +36,15 @@ if (process.env.NODE_ENV === 'development') {
 	});
 } else {
 	require('./www/server.js');
-
 	console.log(`Worker ${process.pid} started`);
 }
 
-
-const logs = path.join(__dirname, 'www', 'public', 'logs');
-fs.mkdir(logs, (err) => {
-	if (err) {
-		if (err.code === 'EEXIST') {
-			console.log(`Folder '${logs}' already exists.`);
+function createDir() {
+	fs.mkdir(logsPath, { recursive: true }, err => {
+		if (!err || (err && err.code === 'EEXIST')) {
+			console.log(`Folder '${logsPath}' created or already exists.`);
 		} else {
 			console.error('Unable to create folder:', err);
 		}
-	} else {
-		console.log(`Folder '${logs}' created successfully!`);
-	}
-});
+	});
+}
