e36f35680f40dfb950ecb55738b5dff0a7e3f2c3
Sefinek <sefinek5916@gmail.com>
2023-09-15 17:18:55 +0200

    Update

--- www/routes/Blocklist.js
+++ www/routes/Blocklist.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const autoIndex = require('express-autoindex');
-const path = require('node:path');
+const path = require('path');
 const increment = require('../middlewares/increment.js');
+const SERVE_FILES = process.env.SERVE_FILES;
 
 const generated = path.join(__dirname, '..', '..', 'blocklist', 'generated');
 const ZeroZeroZeroZero = path.join(__dirname, '..', '..', 'blocklist', 'generated', '0.0.0.0');
@@ -10,13 +11,33 @@ const ZeroZeroZeroZero = path.join(__dirname, '..', '..', 'blocklist', 'generate
 const logs = path.join(__dirname, '..', 'public', 'logs');
 const options = { customTemplate: path.join(__dirname, '..', 'views', 'autoindex', 'default.html'), dirAtTop: true };
 
+function redirectMiddleware(req, res, next) {
+	const urlParts = req.url.split('/');
+	console.log(urlParts);
+	if (urlParts.length === 3) {
+		const ipAddress = urlParts[1];
+		const fileName = urlParts[2];
 
-// Blocklists & logs
-router.use('/generated', increment.blocklist, autoIndex(generated, options), express.static(generated));
+		return res.status(301).redirect(`https://raw.githubusercontent.com/sefinek24/Sefinek-Blocklist-Collection/main/blocklist/generated/${ipAddress}/${fileName}`);
+	}
+
+	next();
+}
+
+// Serve block lists
+if (SERVE_FILES === 'static') {
+	router.use('/generated', increment.blocklist, autoIndex(generated, options), express.static(generated));
+} else if (SERVE_FILES === 'redirect') {
+	router.use('/generated', increment.blocklist, autoIndex(generated, options), redirectMiddleware);
+} else {
+	console.error('Invalid value for the SERVE_FILES environment variable');
+	process.exit(1);
+}
+
+// Logs
 router.use('/logs', autoIndex(logs, options), express.static(logs));
 
 // Deprecated endpoints
 router.get('/generated/0.0.0.0/riotgames.txt', (req, res) => res.sendFile(`${ZeroZeroZeroZero}/sites/riotgames.txt`));
-
 
 module.exports = router;
