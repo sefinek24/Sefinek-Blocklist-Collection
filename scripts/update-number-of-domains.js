07c25f11bb74fae8a7e39170da4488a6b780804a
Sefinek <contact@sefinek.net>
2023-09-24 00:09:25 +0200

    Fixes

--- scripts/update-number-of-domains.js
+++ scripts/update-number-of-domains.js
@@ -1,15 +1,24 @@
 const { readFile, writeFile } = require('node:fs/promises');
 const { join } = require('node:path');
+const kleur = require('kleur');
 const getAllTxtFiles = require('./functions/getAllTxtFiles.js');
 
 (async () => {
+	console.warn(kleur.yellow('[WARN]:'), 'THIS PROCEDURE DEMANDS A SIGNIFICANT AMOUNT OF RAM!\n');
+
 	const blockListDir = join(__dirname, '..', 'blocklist', 'generated');
+	console.warn(kleur.blue('[INFO]:'), `Path: ${blockListDir}`);
+
+	console.warn(kleur.blue('[INFO]:'), 'Searching txt files...');
 	const files = await getAllTxtFiles(blockListDir);
 
+	console.warn(kleur.blue('[INFO]:'), 'Promise.all()');
+	console.warn(kleur.yellow('[WARN]:'), 'This may take a while. Your computer may become unresponsive for a moment.');
 	await Promise.all(files.map(async file => {
+		console.warn(kleur.green('[INFO]:'), `Counting domains in the file: ${file}`);
+
 		const existingDomains = new Set();
 		const fileContents = await readFile(file, 'utf8');
-
 		fileContents.split('\n').forEach(line => {
 			if (line.startsWith('0.0.0.0 ')) {
 				const domain = line.replace(/^0\.0\.0\.0\s+/g, '');
@@ -17,14 +26,21 @@ const getAllTxtFiles = require('./functions/getAllTxtFiles.js');
 			}
 		});
 
-		await writeFile(
-			file,
 
-			fileContents
-				.replace(/^# Total number of network filters: ?(\d*)$/gmu, `# Total number of network filters: ${existingDomains.size}`)
-				.replace('# Count       : N/A', `# Count       : ${existingDomains.size}`),
+		try {
+			console.warn(kleur.green('[INFO]:'), `Saving the file: ${file}`);
 
-			'utf8',
-		);
+			await writeFile(
+				file,
+
+				fileContents
+					.replace(/^# Total number of network filters: ?(\d*)$/gmu, `# Total number of network filters: ${existingDomains.size}`)
+					.replace('# Count       : N/A', `# Count       : ${existingDomains.size}`),
+
+				'utf8',
+			);
+		} catch (err) {
+			console.warn(kleur.red('[ERROR]:'), err.message);
+		}
 	}));
 })();
