e7216d6ec97e6ee6bd38772c60fee9682927eb77
Sefinek <sefinek5916@gmail.com>
2023-09-23 04:25:53 +0200

    0.10.3

--- www/server.js
+++ www/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-const compression = require('compression');
 const path = require('node:path');
 const increment = require('./middlewares/increment.js');
 const logger = require('./middlewares/morgan.js');
@@ -27,7 +26,6 @@ app.use(cors({ origin: true }));
 app.use(helmet({ contentSecurityPolicy: false, crossOriginResourcePolicy: false }));
 app.use(logger.use);
 app.use(limiter);
-app.use(compression());
 
 // Static
 app.use(express.static(path.join(__dirname, 'public')));
