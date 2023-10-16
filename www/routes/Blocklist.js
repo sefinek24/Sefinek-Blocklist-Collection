9909476724a084f17e3cfd0da2b9c9250e043c85
Sefinek <contact@sefinek.net>
2023-10-16 14:08:58 +0200

    Update

--- www/routes/Blocklist.js
+++ www/routes/Blocklist.js
@@ -3,18 +3,22 @@ const router = express.Router();
 const autoIndex = require('express-autoindex');
 const path = require('path');
 const increment = require('../middlewares/increment.js');
+
+// Variables
 const SERVE_FILES = process.env.SERVE_FILES;
 
+// Paths
 const generated = path.join(__dirname, '..', '..', 'blocklist', 'generated');
 const ZeroZeroZeroZero = path.join(__dirname, '..', '..', 'blocklist', 'generated', '0.0.0.0');
-
 const logs = path.join(__dirname, '..', 'public', 'logs');
 const options = { customTemplate: path.join(__dirname, '..', 'views', 'autoindex', 'default.html'), dirAtTop: true };
 
-function redirectMiddleware(req, res) {
+// Functions
+const redirectMiddleware = (req, res) => {
 	const url = req.url;
 	return res.status(301).redirect(`https://raw.githubusercontent.com/sefinek24/Sefinek-Blocklist-Collection/main/blocklist/generated/${url}`);
-}
+};
+
 
 // Serve block lists
 if (SERVE_FILES === 'static') {
@@ -31,5 +35,6 @@ router.use('/logs', autoIndex(logs, options), express.static(logs));
 
 // Deprecated endpoints
 router.get('/generated/0.0.0.0/riotgames.txt', (req, res) => res.sendFile(`${ZeroZeroZeroZero}/sites/riotgames.txt`));
+
 
 module.exports = router;
