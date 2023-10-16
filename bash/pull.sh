3f67b3e8a09a3cb8051429b5e58f04011057d55c
Sefinek <contact@sefinek.net>
2023-10-16 12:53:41 +0200

    Fixes

--- bash/pull.sh
+++ bash/pull.sh
@@ -5,30 +5,35 @@ repo_path="/home/ubuntu/node/Sefinek-Blocklist-Collection"  # Path to the reposi
 logs_dir="$repo_path/www/public/logs"  # Directory to store logs
 output_file="$logs_dir/pull_$(date +'%Y-%m-%d').log"  # Path to the output log file
 
-# Change to the repository directory
-cd "$repo_path" || exit
+# Change to the repository directory or exit if it fails
+cd "$repo_path" || { echo "Error: Could not change to the repository directory"; exit 1; }
 
 # Create the logs directory if it doesn't exist
-mkdir -p "$logs_dir"
+mkdir -p "$logs_dir" || { echo "Error: Could not create the logs directory"; exit 1; }
+
+# Redirect all output to the log file
+exec >> "$output_file" 2>&1
 
 # Write the full date and time to the output file
-echo "========================================== $(date + '%Y-%m-%d %H:%M:%S') ==========================================" >> "$output_file"
-echo "Repository path : $repo_path" >> "$output_file"
-echo "Logs directory  : $logs_dir" >> "$output_file"
-echo "Output file     : $output_file" >> "$output_file"
-echo >> "$output_file"
+echo "========================================== $(date +'%Y-%m-%d %H:%M:%S') =========================================="
+echo "Repository path : $repo_path"
+echo "Logs directory  : $logs_dir"
+echo "Output file     : $output_file"
+echo
 
-# Git
-git --version >> "$output_file" 2>&1  # Check Git version and append it to the output file
-echo >> "$output_file"
+# Check Git version and append it to the output file
+git --version
 
-git fetch >> "$output_file" 2>&1  # Fetch the latest changes from the remote repository and append output to the log file
-git pull >> "$output_file" 2>&1  # Pull the latest changes from the remote repository and append output to the log file
+# Fetch the latest changes from the remote repository and append output to the log file
+git fetch
 
-echo >> "$output_file" 2>&1
-echo >> "$output_file" 2>&1
-echo "Success! Date: $(date +'%Y-%m-%d %H:%M:%S')" >> "$output_file" 2>&1  # Add a success message with the current date and time to the output file
-echo >> "$output_file" 2>&1
+# Pull the latest changes from the remote repository and append output to the log file
+git pull
 
-# Final
-echo "Done. Output file has been created or updated: $output_file"  # Print a message indicating that the script has finished and the output file location
+echo
+echo
+echo "Success! Date: $(date +'%Y-%m-%d %H:%M:%S')"  # Add a success message with the current date and time to the output file
+echo
+
+# Print a message indicating that the script has finished and the output file location
+echo "Done. Output file has been created or updated: $output_file"
