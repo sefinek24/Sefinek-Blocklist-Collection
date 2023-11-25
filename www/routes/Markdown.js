c0032ce67431ec30f4db6c661e60a5ae882eec57
Sefinek <contact@sefinek.net>
2023-11-25 04:43:08 +0100

    Update for better SEO

--- www/routes/Markdown.js
+++ www/routes/Markdown.js
@@ -11,6 +11,12 @@ const options = { customTemplate: path.join(__dirname, '..', 'views', 'autoindex
 const Marked = require('marked');
 Marked.use({ pedantic: false, gfm: true });
 
+// Regex
+const TITLE_REGEX = /^######\s*Title:\s*(.*)/mi;
+const DESC_REGEX = /^######\s*Description:\s*(.*)/mi;
+const TAGS_REGEX = /^######\s*Tags:\s*(.*)/mi;
+const CANONICAL_REGEX = /^######\s*Canonical:\s*(.*)/mi;
+
 
 router.get('/markdown', (req, res) => res.render('markdown.ejs', { version }));
 router.use('/markdown/lists', autoIndex(path.join(__dirname, '..', '..', 'docs', 'lists'), options));
@@ -29,7 +35,14 @@ router.use('/viewer/:type', async (req, res) => {
 		const mdFile = await fs.promises.readFile(fullPath, 'utf8');
 		const html = Marked.parse(mdFile);
 
-		res.render('markdown-viewer.ejs', { version, html, file });
+		res.render('markdown-viewer.ejs', { version,
+			html,
+			file,
+			title: mdFile.match(TITLE_REGEX) ? mdFile.match(TITLE_REGEX)[1] : undefined,
+			desc: mdFile.match(DESC_REGEX) ? mdFile.match(DESC_REGEX)[1] : undefined,
+			tags: mdFile.match(TAGS_REGEX) ? mdFile.match(TAGS_REGEX)[1] : undefined,
+			canonical: mdFile.match(CANONICAL_REGEX) ? mdFile.match(CANONICAL_REGEX)[1].trim() : undefined,
+		});
 	} catch (err) {
 		res.sendStatus(500);
 		console.error(err);
