6b9fe38027bba21174f7cfd870ae864b1450d85e
Sefinek <contact@sefinek.net>
2023-11-28 10:21:25 +0100

    Fixes
    Signed-off-by: Sefinek <contact@sefinek.net>

--- www/public/js/websocket.js
+++ www/public/js/websocket.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket(NODE_ENV === 'production' ? 'wss://blocklist.sefinek.net/wsapp' : `ws://127.0.0.1:${WS_PORT}`);
+const socket = new WebSocket(WEBSOCKET);
 
 socket.onopen = () => {
 	console.log('WebSocket connection established.');
