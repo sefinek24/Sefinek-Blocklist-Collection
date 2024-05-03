c3c85c58957405c8027e4faf9ab4259164c35f0a
Sefinek Actions <contact@sefinek.net>
2024-05-03 13:39:13 +0200

    sefinek

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,9 +5,9 @@ module.exports = {
 
 		log_date_format       : 'HH:mm:ss.SSS DD.MM.YYYY',
 		merge_logs            : true,
-		log_file		      : '/home/ubuntu/logs/www/blocklist.sefinek.net/combined.log',
-		out_file              : '/home/ubuntu/logs/www/blocklist.sefinek.net/out.log',
-		error_file            : '/home/ubuntu/logs/www/blocklist.sefinek.net/error.log',
+		log_file		      : '/home/sefinek/logs/www/blocklist.sefinek.net/combined.log',
+		out_file              : '/home/sefinek/logs/www/blocklist.sefinek.net/out.log',
+		error_file            : '/home/sefinek/logs/www/blocklist.sefinek.net/error.log',
 
 		max_restarts          : 4,
 		restart_delay         : 6000,
