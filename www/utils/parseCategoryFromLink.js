d32e6863921bf674769fb1cd3e3662256a8ec64a
Sefinek <contact@sefinek.net>
2024-07-05 07:59:27 +0200

    Stats

--- www/utils/parseCategoryFromLink.js
+++ www/utils/parseCategoryFromLink.js
@@ -1,22 +1,18 @@
+const categories = {
+	'0.0.0.0': '0000',
+	'127.0.0.1': '127001'
+};
+
 module.exports = url => {
 	const segments = url.split('/');
 	const fourthSegment = segments[3];
 	const fileName = segments[segments.length - 1];
+	const isValidFile = fileName.endsWith('.txt') || fileName.endsWith('.conf');
 
-	let category;
-	switch (fourthSegment) {
-	case '0.0.0.0':
-		category = '0000';
-		break;
-	case '127.0.0.1':
-		category = '127001';
-		break;
-	default:
-		category = fourthSegment;
-	}
-
-	let isValidFile = fileName.endsWith('.txt') || fileName.endsWith('.conf');
-	if (!isValidFile) isValidFile = false;
-
-	return { url, array: segments, category, fileName: isValidFile ? fileName.replace(/[.]/, '-') : null };
+	return {
+		url,
+		array: segments,
+		category: categories[fourthSegment] || fourthSegment,
+		fileName: isValidFile ? fileName.replace(/\./g, '-') : null
+	};
 };
