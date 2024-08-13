ecca0095725177de7120c0e0eafdec8472dab29e
Sefinek <contact@sefinek.net>
2024-08-14 00:40:47 +0200

    Update

--- scripts/generate/file-processor/scripts/data.js
+++ scripts/generate/file-processor/scripts/data.js
@@ -1,83 +1,83 @@
 exports.fileUrls = [
 	// Misc
-	// { url: 'https://raw.githubusercontent.com/shreshta-labs/newly-registered-domains/main/nrd-1w.csv', name: 'shreshta-labs_nrd-1w.txt' },
-	// { url: 'https://github.com/spaze/domains/raw/main/tld-cz.txt', name: 'spaze_tld-cz.txt' },
+	{ url: 'https://raw.githubusercontent.com/shreshta-labs/newly-registered-domains/main/nrd-1w.csv', name: 'shreshta-labs_nrd-1w.txt' },
+	{ url: 'https://github.com/spaze/domains/raw/main/tld-cz.txt', name: 'spaze_tld-cz.txt' },
 
-	// // xRuffKez
-	// { url: 'https://raw.githubusercontent.com/xRuffKez/NRD/main/nrd-30day_part1.txt', name: 'xRuffKez_nrd-30day-part1.txt' },
-	// { url: 'https://raw.githubusercontent.com/xRuffKez/NRD/main/nrd-30day_part2.txt', name: 'xRuffKez_nrd-30day-part2.txt' },
-	//
-	// // whoisds
-	// { url: 'https://whoisds.com/whois-database/newly-registered-domains/MjAyNC0wOC0xMi56aXA=/nrd', name: 'whoisds1.zip' },
-	// { url: 'https://whoisds.com/whois-database/newly-registered-domains/MjAyNC0wOC0xMS56aXA=/nrd', name: 'whoisds2.zip' },
-	// { url: 'https://whoisds.com/whois-database/newly-registered-domains/MjAyNC0wOC0xMC56aXA=/nrd', name: 'whoisds3.zip' },
-	// { url: 'https://whoisds.com/whois-database/newly-registered-domains/MjAyNC0wOC0wOS56aXA=/nrd', name: 'whoisds4.zip' },
-	//
-	// // tb0hdan
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/generic_lgbt/domain2multi-lgbt00.txt.xz', name: 'tb0hdan_generic-lgbt.xz' }
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/generic_gay/domain2multi-gay00.txt.xz', name: 'tb0hdan_generic-gay.xz' },
-	//
+	// xRuffKez
+	{ url: 'https://raw.githubusercontent.com/xRuffKez/NRD/main/nrd-30day_part1.txt', name: 'xRuffKez_nrd-30day-part1.txt' },
+	{ url: 'https://raw.githubusercontent.com/xRuffKez/NRD/main/nrd-30day_part2.txt', name: 'xRuffKez_nrd-30day-part2.txt' },
+
+	// whoisds
+	{ url: 'https://whoisds.com/whois-database/newly-registered-domains/MjAyNC0wOC0xMi56aXA=/nrd', name: 'whoisds1.zip' },
+	{ url: 'https://whoisds.com/whois-database/newly-registered-domains/MjAyNC0wOC0xMS56aXA=/nrd', name: 'whoisds2.zip' },
+	{ url: 'https://whoisds.com/whois-database/newly-registered-domains/MjAyNC0wOC0xMC56aXA=/nrd', name: 'whoisds3.zip' },
+	{ url: 'https://whoisds.com/whois-database/newly-registered-domains/MjAyNC0wOC0wOS56aXA=/nrd', name: 'whoisds4.zip' },
+
+	// tb0hdan
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/generic_lgbt/domain2multi-lgbt00.txt.xz', name: 'tb0hdan_generic-lgbt.xz' },
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/generic_gay/domain2multi-gay00.txt.xz', name: 'tb0hdan_generic-gay.xz' },
+
 	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/germany/domain2multi-de00.txt.xz', name: 'tb0hdan_d2m-de00.xz' },
-	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/germany/domain2multi-de01.txt.xz', name: 'tb0hdan_d2m-de01.xz' }
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/germany/domain2multi-de02.txt.xz', name: 'tb0hdan_d2m-de02.xz' },
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/germany/domain2multi-de03.txt.xz', name: 'tb0hdan_d2m-de03.xz' },
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/germany/domain2multi-de04.txt.xz', name: 'tb0hdan_d2m-de04.xz' },
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/germany/domain2multi-de05.txt.xz', name: 'tb0hdan_d2m-de05.xz' },
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/germany/domain2multi-de06.txt.xz', name: 'tb0hdan_d2m-de06.xz' },
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/germany/domain2multi-de07.txt.xz', name: 'tb0hdan_d2m-de07.xz' },
-	//
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/poland/domain2multi-pl00.txt.xz', name: 'tb0hdan_d2m-pl00.xz' },
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/poland/domain2multi-pl01.txt.xz', name: 'tb0hdan_d2m-pl01.xz' },
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/poland/domain2multi-pl02.txt.xz', name: 'tb0hdan_d2m-pl02.xz' },
-	//
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/united_states/domain2multi-us00.txt.xz', name: 'tb0hdan_d2m-us00.xz' },
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/united_kingdom/domain2multi-uk00.txt.xz', name: 'tb0hdan_d2m-uk00.xz' },
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/united_kingdom/domain2multi-uk01.txt.xz', name: 'tb0hdan_d2m-uk01.xz' },
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/united_kingdom/domain2multi-uk02.txt.xz', name: 'tb0hdan_d2m-uk02.xz' },
-	//
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/venezuela/domain2multi-ve00.txt.xz', name: 'tb0hdan_d2m-ve00.xz' },
-	//
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/australia/domain2multi-au00.txt.xz', name: 'tb0hdan_d2m-au00.xz' },
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/australia/domain2multi-au01.txt.xz', name: 'tb0hdan_d2m-au01.xz' },
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/australia/domain2multi-au02.txt.xz', name: 'tb0hdan_d2m-au02.xz' },
-	//
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/austria/domain2multi-at00.txt.xz', name: 'tb0hdan_d2m-at00.xz' },
-	//
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/south_africa/domain2multi-za00.txt.xz', name: 'tb0hdan_d2m-za00.xz' },
-	//
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/canada/domain2multi-ca00.txt.xz', name: 'tb0hdan_d2m-ca00.xz' },
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/canada/domain2multi-ca01.txt.xz', name: 'tb0hdan_d2m-ca01.xz' },
-	//
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/chile/domain2multi-cl00.txt.xz', name: 'tb0hdan_d2m-cl00.xz' },
-	//
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/new_zealand/domain2multi-nz00.txt.xz', name: 'tb0hdan_d2m-nz00.xz' },
-	//
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/philippines/domain2multi-ph00.txt.xz', name: 'tb0hdan_d2m-ph00.xz' },
-	//
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/israel/domain2multi-il00.txt.xz', name: 'tb0hdan_d2m-il00.xz' },
-	//
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/thailand/domain2multi-th00.txt.xz', name: 'tb0hdan_d2m-th00.xz' },
-	//
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/brazil/domain2multi-br00.txt.xz', name: 'tb0hdan_d2m-br00.xz' },
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/brazil/domain2multi-br01.txt.xz', name: 'tb0hdan_d2m-br01.xz' },
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/brazil/domain2multi-br02.txt.xz', name: 'tb0hdan_d2m-br02.xz' },
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/brazil/domain2multi-br03.txt.xz', name: 'tb0hdan_d2m-br03.xz' },
-	//
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/sweden/domain2multi-se00.txt.xz', name: 'tb0hdan_d2m-se00.xz' },
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/sweden/domain2multi-se01.txt.xz', name: 'tb0hdan_d2m-se01.xz' },
-	//
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/spain/domain2multi-es00.txt.xz', name: 'tb0hdan_d2m-es00.xz' },
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/spain/domain2multi-es01.txt.xz', name: 'tb0hdan_d2m-es01.xz' },
-	//
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/france/domain2multi-fr00.txt.xz', name: 'tb0hdan_d2m-fr00.xz' },
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/france/domain2multi-fr01.txt.xz', name: 'tb0hdan_d2m-fr01.xz' },
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/france/domain2multi-fr02.txt.xz', name: 'tb0hdan_d2m-fr02.xz' },
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/france/domain2multi-fr03.txt.xz', name: 'tb0hdan_d2m-fr03.xz' },
-	//
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/netherlands/domain2multi-nl00.txt.xz', name: 'tb0hdan_d2m-nl00.xz' },
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/netherlands/domain2multi-nl01.txt.xz', name: 'tb0hdan_d2m-nl01.xz' },
-	//
-	// { url: 'https://github.com/tb0hdan/domains/raw/master/data/iceland/domain2multi-is00.txt.xz', name: 'tb0hdan_d2m-is00.xz' }
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/germany/domain2multi-de01.txt.xz', name: 'tb0hdan_d2m-de01.xz' },
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/germany/domain2multi-de02.txt.xz', name: 'tb0hdan_d2m-de02.xz' },
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/germany/domain2multi-de03.txt.xz', name: 'tb0hdan_d2m-de03.xz' },
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/germany/domain2multi-de04.txt.xz', name: 'tb0hdan_d2m-de04.xz' },
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/germany/domain2multi-de05.txt.xz', name: 'tb0hdan_d2m-de05.xz' },
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/germany/domain2multi-de06.txt.xz', name: 'tb0hdan_d2m-de06.xz' },
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/germany/domain2multi-de07.txt.xz', name: 'tb0hdan_d2m-de07.xz' },
+
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/poland/domain2multi-pl00.txt.xz', name: 'tb0hdan_d2m-pl00.xz' },
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/poland/domain2multi-pl01.txt.xz', name: 'tb0hdan_d2m-pl01.xz' },
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/poland/domain2multi-pl02.txt.xz', name: 'tb0hdan_d2m-pl02.xz' },
+
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/united_states/domain2multi-us00.txt.xz', name: 'tb0hdan_d2m-us00.xz' },
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/united_kingdom/domain2multi-uk00.txt.xz', name: 'tb0hdan_d2m-uk00.xz' },
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/united_kingdom/domain2multi-uk01.txt.xz', name: 'tb0hdan_d2m-uk01.xz' },
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/united_kingdom/domain2multi-uk02.txt.xz', name: 'tb0hdan_d2m-uk02.xz' },
+
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/venezuela/domain2multi-ve00.txt.xz', name: 'tb0hdan_d2m-ve00.xz' },
+
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/australia/domain2multi-au00.txt.xz', name: 'tb0hdan_d2m-au00.xz' },
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/australia/domain2multi-au01.txt.xz', name: 'tb0hdan_d2m-au01.xz' },
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/australia/domain2multi-au02.txt.xz', name: 'tb0hdan_d2m-au02.xz' },
+
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/austria/domain2multi-at00.txt.xz', name: 'tb0hdan_d2m-at00.xz' },
+
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/south_africa/domain2multi-za00.txt.xz', name: 'tb0hdan_d2m-za00.xz' },
+
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/canada/domain2multi-ca00.txt.xz', name: 'tb0hdan_d2m-ca00.xz' },
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/canada/domain2multi-ca01.txt.xz', name: 'tb0hdan_d2m-ca01.xz' },
+
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/chile/domain2multi-cl00.txt.xz', name: 'tb0hdan_d2m-cl00.xz' },
+
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/new_zealand/domain2multi-nz00.txt.xz', name: 'tb0hdan_d2m-nz00.xz' },
+
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/philippines/domain2multi-ph00.txt.xz', name: 'tb0hdan_d2m-ph00.xz' },
+
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/israel/domain2multi-il00.txt.xz', name: 'tb0hdan_d2m-il00.xz' },
+
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/thailand/domain2multi-th00.txt.xz', name: 'tb0hdan_d2m-th00.xz' },
+
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/brazil/domain2multi-br00.txt.xz', name: 'tb0hdan_d2m-br00.xz' },
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/brazil/domain2multi-br01.txt.xz', name: 'tb0hdan_d2m-br01.xz' },
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/brazil/domain2multi-br02.txt.xz', name: 'tb0hdan_d2m-br02.xz' },
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/brazil/domain2multi-br03.txt.xz', name: 'tb0hdan_d2m-br03.xz' },
+
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/sweden/domain2multi-se00.txt.xz', name: 'tb0hdan_d2m-se00.xz' },
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/sweden/domain2multi-se01.txt.xz', name: 'tb0hdan_d2m-se01.xz' },
+
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/spain/domain2multi-es00.txt.xz', name: 'tb0hdan_d2m-es00.xz' },
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/spain/domain2multi-es01.txt.xz', name: 'tb0hdan_d2m-es01.xz' },
+
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/france/domain2multi-fr00.txt.xz', name: 'tb0hdan_d2m-fr00.xz' },
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/france/domain2multi-fr01.txt.xz', name: 'tb0hdan_d2m-fr01.xz' },
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/france/domain2multi-fr02.txt.xz', name: 'tb0hdan_d2m-fr02.xz' },
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/france/domain2multi-fr03.txt.xz', name: 'tb0hdan_d2m-fr03.xz' },
+
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/netherlands/domain2multi-nl00.txt.xz', name: 'tb0hdan_d2m-nl00.xz' },
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/netherlands/domain2multi-nl01.txt.xz', name: 'tb0hdan_d2m-nl01.xz' },
+
+	{ url: 'https://github.com/tb0hdan/domains/raw/master/data/iceland/domain2multi-is00.txt.xz', name: 'tb0hdan_d2m-is00.xz' }
 ];
 
 exports.CATEGORIES = [
@@ -92,7 +92,7 @@ exports.CATEGORIES = [
 		title: 'Blocks most LGBTQ+ websites or those that promote LGBTQ+ content',
 		category: 'LGBTQ+',
 		regex: /n(?:ie(?:konformistyczn[ay]|binarn[ay])|onconforming)|(?:queerplatoni|poliamory)czny|alloromantyczn[ay]|multi\\-seksualny|skoliose(?:ksualny|xual)|demi(?:dziewczyna|chłopiec)|(?:(?:(?:(?:trans|pan)|allo)|poli)|demi)seksualn[ay]|inter(?:seksualn[aiy]|gender)|queer(?:platonic)?|omnise(?:ksualny|xual)|a(?:llo)?romantic|trzecia\\-plec|(?:(?:third\\-|(?:(?:(?:trans|a)|bi)|cis))|xeno)gender|aromantyczn[aiy]|(?:pangenderow|interplciow|ksenoplciow|(?:tranzyt|dwuduch)ow|(?:dwu|bez)płciow|polyamor|nonbinar|demibo|ga)y|ge(?:nderfluid|j(?:owski|e))|genderqueer|(?:bi|a)seksualn[aiy]|(?:(?:(?:(?:(?:(?:(?:(?:trans|pan)|allo)|demi)|a)|bi)|tran)|poly)|multi)sexual|two\\-spirit|(?:poliamori|lesbijsk)a|(?:cispłci|trans)ow[ay]|pangender|lesbi(?:j(?:ek|k[ai])|an)|demigirl|queers|trans|pride|lgbt/gi,
-		// grex "lgbt" "pride" "gay" "geje" "gejowski" "lesbian" "lesbijka" "lesbijki" "lesbijek" "lesbijska" "bisexual" "biseksualny" "biseksualna" "biseksualni" "transgender" "trans" "transowy" "transowa" "transseksualny" "transseksualna" "transexual" "transsexual" "transowy" "tranzytowy" "nonbinary" "niebinarny" "niebinarna" "asexual" "aseksualny" "aseksualna" "aseksualni" "pansexual" "panseksualny" "panseksualna" "aromantic" "aromantyczny" "aromantyczna" "aromantyczni" "demisexual" "demiseksualny" "demiseksualna" "cisgender" "cispłciowy" "cispłciowa" "genderfluid" "genderqueer" "queer" "queers" "lesbijka" "lesbijki" "aseksualny" "aseksualna" "interseksualny" "interseksualna" "interseksualni" "polyamory" "poliamoria" "poliamoryczny" "polysexual" "poliseksualny" "poliseksualna" "bigender" "dwupłciowy" "agender" "bezpłciowy" "two-spirit" "dwuduchowy" "allosexual" "alloseksualny" "alloseksualna" "alloromantic" "alloromantyczny" "alloromantyczna" "nonconforming" "niekonformistyczny" "niekonformistyczna" "pangender" "pangenderowy" "demiboy" "demichłopiec" "demigirl" "demidziewczyna" "intergender" "interplciowy" "multisexual" "multi-seksualny" "omnisexual" "omniseksualny" "queerplatonic" "queerplatoniczny" "skoliosexual" "skolioseksualny" "third-gender" "trzecia-plec" "xenogender" "ksenoplciowy"
+		// grex "lgbt" "pride" "gay" "geje" "gejowski" "lesbian" "lesbijka" "lesbijki" "lesbijek" "lesbijska" "bisexual" "biseksualny" "biseksualna" "biseksualni" "transgender" "transseksualista" "transseksualistka" "transowa" "transseksualny" "transseksualna" "transexual" "transsexual" "transexual" "transowy" "tranzytowy" "nonbinary" "niebinarny" "niebinarna" "asexual" "aseksualny" "aseksualna" "aseksualni" "pansexual" "panseksualny" "panseksualna" "aromantic" "aromantyczny" "aromantyczna" "aromantyczni" "demisexual" "demiseksualny" "demiseksualna" "cisgender" "cispłciowy" "cispłciowa" "genderfluid" "genderqueer" "queer" "queers" "lesbijka" "lesbijki" "aseksualny" "aseksualna" "interseksualny" "interseksualna" "interseksualni" "polyamory" "poliamoria" "poliamoryczny" "polysexual" "poliseksualny" "poliseksualna" "bigender" "dwupłciowy" "agender" "bezpłciowy" "two-spirit" "dwuduchowy" "allosexual" "alloseksualny" "alloseksualna" "alloromantic" "alloromantyczny" "alloromantyczna" "nonconforming" "niekonformistyczny" "niekonformistyczna" "pangender" "pangenderowy" "demiboy" "demichłopiec" "demigirl" "demidziewczyna" "intergender" "interplciowy" "multisexual" "multi-seksualny" "omnisexual" "omniseksualny" "queerplatonic" "queerplatoniczny" "skoliosexual" "skolioseksualny" "third-gender" "trzecia-plec" "xenogender" "ksenoplciowy"
 		file: 'sites/lgbtqplus2.txt'
 	},
 	{
