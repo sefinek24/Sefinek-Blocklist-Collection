55f2c1af480d0e8fb4d393c9aae1082116d04c70
Sefinek <contact@sefinek.net>
2024-08-19 00:42:50 +0200

    Update pages etc.

--- www/cluster/connectToDatabase.js
+++ www/cluster/connectToDatabase.js
@@ -4,8 +4,8 @@ module.exports = async () => {
 	try {
 		await mongoose.connect(process.env.MONGODB_URL);
 		console.log('Connected to MongoDB');
-	} catch (error) {
-		console.error('Error connecting to MongoDB:', error);
+	} catch (err) {
+		console.error('Error connecting to MongoDB:', err);
 		process.exit(1);
 	}
 };
