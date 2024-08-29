3b1c744c4cb3a01391b7d67771fc072839a55414
Sefinek <contact@sefinek.net>
2024-08-29 15:54:02 +0200

    Delete `git --version`

--- bash/pull.sh
+++ bash/pull.sh
@@ -25,9 +25,6 @@ mkdir -p "$logs_dir"
     if [ -d "$repo_path" ]; then
         cd "$repo_path" || exit
 
-        git --version
-        echo
-
         if git fetch && git pull; then
             npm install --omit=dev
             pm2 restart blocklist
