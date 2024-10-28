30021a812339f2bd9942a8b2478c50781dc7f57d
Sefinek <contact@sefinek.net>
2024-10-28 20:54:49 +0100

    Cleanup

--- www/utils/time.js
+++ www/utils/time.js
@@ -15,22 +15,12 @@ exports.full = seconds => {
 	const remainingSeconds = Math.floor(seconds % 60);
 
 	let formattedTime = '';
-	if (days > 0) {
-		formattedTime += `${days} ${days === 1 ? 'day' : 'days'}, `;
-	}
-	if (hours > 0) {
-		formattedTime += `${hours} ${hours === 1 ? 'hour' : 'hours'}, `;
-	}
-	if (minutes > 0) {
-		formattedTime += `${minutes} ${minutes === 1 ? 'minute' : 'minutes'}, `;
-	}
-	if (remainingSeconds > 0) {
-		formattedTime += `${remainingSeconds} ${remainingSeconds === 1 ? 'second' : 'seconds'}`;
-	}
+	if (days > 0) formattedTime += `${days} ${days === 1 ? 'day' : 'days'}, `;
+	if (hours > 0) formattedTime += `${hours} ${hours === 1 ? 'hour' : 'hours'}, `;
+	if (minutes > 0) formattedTime += `${minutes} ${minutes === 1 ? 'minute' : 'minutes'}, `;
+	if (remainingSeconds > 0) formattedTime += `${remainingSeconds} ${remainingSeconds === 1 ? 'second' : 'seconds'}`;
 
-	// Trim any trailing comma and space
 	formattedTime = formattedTime.replace(/,\s*$/, '');
-
 	return formattedTime;
 };
 
