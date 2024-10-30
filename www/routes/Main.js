b6a0b957ed81a3c4a3dcde087d4c477d40456d00
Sefinek <contact@sefinek.net>
2024-10-30 22:11:14 +0100

    test

--- www/routes/Main.js
+++ www/routes/Main.js
@@ -5,6 +5,5 @@ const MainController = require('../controllers/Main.js');
 
 router.get('/', MainController.index);
 router.get('/update-schedule', MainController.updateSchedule);
-router.get('/cf', (req, res) => res.send(req.ip));
 
 module.exports = router;
