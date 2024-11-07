a0b5eccec6499f48ec29438afae15b46e4d43c51
Sefinek <contact@sefinek.net>
2024-11-07 04:02:27 +0100

    PM2 cfg update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,26 @@
 module.exports = {
 	apps: [{
-		name                  : 'blocklist',
-		script                : './index.js',
+		name: 'blocklist',
+		script: './index.js',
 
-		log_date_format       : 'HH:mm:ss.SSS DD.MM.YYYY',
-		merge_logs            : true,
-		log_file		      : '/home/sefinek/logs/www/blocklist.sefinek.net/combined.log',
-		out_file              : '/home/sefinek/logs/www/blocklist.sefinek.net/out.log',
-		error_file            : '/home/sefinek/logs/www/blocklist.sefinek.net/error.log',
+		// Logging configuration
+		log_date_format: 'HH:mm:ss.SSS DD.MM.YYYY',
+		merge_logs: true,
+		log_file: '/home/sefinek/logs/www/blocklist.sefinek.net/combined.log',
+		out_file: '/home/sefinek/logs/www/blocklist.sefinek.net/out.log',
+		error_file: '/home/sefinek/logs/www/blocklist.sefinek.net/error.log',
 
-		max_restarts          : 4,
-		restart_delay         : 6000,
-		wait_ready            : true,
+		// Application restart policy settings
+		wait_ready: true,
+		autorestart: true,
+		max_restarts: 10,
+		min_uptime: 20000,
+		restart_delay: 5000,
+		exp_backoff_restart_delay: 3000,
 
-		// instances             : 'max',
-		// exec_mode             : 'cluster',
+		// Environment variables configuration
+		env: {
+			NODE_ENV: 'production',
+		},
 	}],
 };
