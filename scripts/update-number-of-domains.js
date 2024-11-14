6e182e2595689072119f2e0db9a4154865bb21b8
Sefinek <contact@sefinek.net>
2024-11-14 19:02:27 +0100

    Update update-number-of-domains.js

--- scripts/update-number-of-domains.js
+++ scripts/update-number-of-domains.js
@@ -1,47 +1,40 @@
 const { writeFile } = require('node:fs/promises');
 const { createReadStream } = require('node:fs');
-const { red, yellow, blue, green } = require('kleur');
+const { red, blue, green } = require('kleur');
 const readline = require('readline');
 const { join } = require('node:path');
 const getAllTxtFiles = require('./utils/getAllTxtFiles.js');
 
-const ignoreQuestion = process.argv.includes('--ignore-question');
-
 const createUpdatedContents = (lines, domainCount) => {
 	return lines.join('\n')
-		.replace(/^# Count: ?(\d*)$/gmu, `# Count: ${domainCount || 'N/A'}`)
-		.replace(/^# Entries: ?(\d*)$/gmu, `# Entries: ${domainCount || 'N/A'}`)
+		.replace(/^# Count: ?(\d*)$/gim, `# Count: ${domainCount || 'N/A'}`)
+		.replace(/^# Entries: ?(\d*)$/gim, `# Entries: ${domainCount || 'N/A'}`)
+		.replace(/^(# Entries:\s*)(\d+)$/gim, (_, prefix) => `${prefix}${domainCount || 'N/A'}`)
 		.replace('# Count       : N/A', `# Count       : ${domainCount || 'N/A'}`)
-		.replace(/^# Number of entries: ?(\d*)$/gmu, `# Number of entries: ${domainCount || 'N/A'}`)
-		.replace(/^# Number of unique domains: ?(\d*)$/gmu, `# Number of unique domains: ${domainCount || 'N/A'}`)
-		.replace(/^# Total number of network filters: ?(\d*)$/gmu, `# Total number of network filters: ${domainCount || 'N/A'}`);
+		.replace(/^# Number of entries: ?(\d*)$/gim, `# Number of entries: ${domainCount || 'N/A'}`)
+		.replace(/^# Number of unique domains: ?(\d*)$/gim, `# Number of unique domains: ${domainCount || 'N/A'}`)
+		.replace(/^# Total number of network filters: ?(\d*)$/gim, `# Total number of network filters: ${domainCount || 'N/A'}`);
 };
 
 const processFile = async file => {
-	const readStream = createReadStream(file, 'utf8');
-	const fileInterface = readline.createInterface({ input: readStream });
-
 	let domainCount = 0;
-	const updatedContent = [];
-	try {
-		for (const line of fileInterface) {
-			if (line.startsWith('0.0.0.0 ')) domainCount++;
-			updatedContent.push(line);
-		}
-	} finally {
-		fileInterface.close();
-		readStream.close();
+	const lines = [];
+
+	const rl = readline.createInterface({ input: createReadStream(file, 'utf8'), crlfDelay: Infinity });
+	for await (const line of rl) {
+		if (line.startsWith('0.0.0.0 ')) domainCount++;
+		lines.push(line);
 	}
 
-	const updatedFileContents = createUpdatedContents(updatedContent, domainCount);
+	const updatedFileContents = createUpdatedContents(lines, domainCount);
 	await writeFile(file, updatedFileContents, 'utf8');
-
-	console.log(green('[INFO]:'), `Saved: ${file} (${domainCount} domains)`);
+	const formattedCount = domainCount.toLocaleString('en-US', { minimumIntegerDigits: 8, useGrouping: true }).replace(/,/g, ' ');
+	console.log(green('[INFO]:'), `${formattedCount} domains: ${file}`);
 };
 
-const proceedWithProcedure = async () => {
+(async () => {
 	const blockListDir = join(__dirname, '..', 'blocklists', 'templates');
-	console.log(blue('[INFO]:'), `Path: ${blockListDir}`);
+	console.log(blue('[INFO]:'), blockListDir);
 
 	try {
 		const files = await getAllTxtFiles(blockListDir);
@@ -51,22 +44,6 @@ const proceedWithProcedure = async () => {
 			await processFile(file);
 		}
 	} catch (err) {
-		console.error(red('[ERROR]:'), err.message);
+		console.error(red('[FAIL]:'), err.message);
 	}
-};
-
-(async () => {
-	if (!ignoreQuestion) {
-		const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
-		const answer = await new Promise(resolve => {
-			rl.question(`${red('[WARN]: THIS PROCEDURE DEMANDS A SIGNIFICANT AMOUNT OF RAM!')}\n\n> ${yellow('Are you sure? [Yes/no]: ')}`, resolve);
-		});
-		rl.close();
-
-		if (answer.toLowerCase() === 'no') {
-			return console.log(red('[INFO]: Procedure aborted by the user.'));
-		}
-	}
-
-	await proceedWithProcedure();
 })();
