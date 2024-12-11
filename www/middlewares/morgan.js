ca38651aee56b5d3879e01bf7927cf09c1bd78e3
Sefinek <contact@sefinek.net>
2024-12-11 15:14:21 +0100

    :remote-addr

--- www/middlewares/morgan.js
+++ www/middlewares/morgan.js
@@ -3,4 +3,4 @@ const morgan = require('morgan');
 const UPTIME_BOT_UA = 'Better Uptime Bot Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36';
 const skip = req => req.headers['user-agent'] === UPTIME_BOT_UA;
 
-module.exports = morgan('[:status :method :response-time ms] :url :user-agent ":referrer"', { skip });
+module.exports = morgan('[:status :method :response-time ms] :url :user-agent :remote-addr ":referrer"', { skip });
